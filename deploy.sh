2dc9f1950e3aad8a04dace6f96e9e00046d13fc3
abdo224 <djawedbessa@gmail.com>
2024-03-28 23:12:45 +0000

    advaced version

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
-sudo chmod 777 /etc/rancher/k3s/k3s.yaml 
-cd K8s
-kubectl apply -f redis.yaml
-kubectl apply -f redis-node.yml
-kubectl apply -f react.yml
-kubectl create ns monitoring 
-helm install prometheus prometheus-community/kube-prometheus-stack -n monitoring 
